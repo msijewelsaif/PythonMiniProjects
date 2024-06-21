9732d86416a1358b20c8de831d936560cf0890bb
Md Saiful Islam <94510295+msijewelsaif@users.noreply.github.com>
2024-06-21 22:45:26 +0600

    add Homepage folder

---
+++ Homepage/static/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const exploreBtn = document.getElementById('exploreBtn');
+    exploreBtn.addEventListener('click', () => {
+        alert('Welcome to the amazing content!');
+    });
+});
